7f594c9791099430b3f8f5d38aa2be4d7af34068
Pier-Hugues Pellerin <phpellerin@gmail.com>
2022-03-25 20:03:21 -0400

    Play with the zerolog logger
    Add future notes implementations for filtering.

--- cmd/vif/main.go
+++ cmd/vif/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"go.elastic.co/ecszerolog"
+	"os"
+)
 
+func main() {
+	// TODO(ph) Add build information/version
+	// TODO(ph) Make sure we can link logging to the agent policy.
+	// TODO(ph) use severity hook with the level.Discard to filter specific event
+	logger := ecszerolog.New(os.Stdout)
+	log.Logger = logger
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	log.Info().Msg("hello world")
+	log.Debug().Msg("Have some fun")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/ph/vif
 
 go 1.17
+
+require (
+	github.com/magefile/mage v1.11.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rs/zerolog v1.26.1 // indirect
+	go.elastic.co/ecszerolog v0.1.0 // indirect
+)
